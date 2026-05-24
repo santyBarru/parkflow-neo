402d1d766289b57548891229e36eb64662c34597
santyBarru <santiago.barrera2007@gmail.com>
2026-05-24 10:25:52 -0500

    test: fix Cypress tests for new NEO login UI - 3 passing

--- parkflow-frontend/cypress/e2e/parkflow.cy.js
+++ parkflow-frontend/cypress/e2e/parkflow.cy.js
@@ -1,34 +1,27 @@
-describe('ParkFlow NEO - Pruebas de GUI', () => {
-  
+describe("ParkFlow NEO - Pruebas de GUI", () => {
   beforeEach(() => {
-    cy.visit('http://localhost:3000')
-  })
+    cy.visit("http://localhost:3000");
+    cy.wait(3500);
+  });
 
-  it('Debe cargar la página de login', () => {
-    cy.url().should('include', 'localhost:3000')
-    cy.get('input').should('exist')
-  })
+  it("Debe cargar la página de login", () => {
+    cy.url().should("include", "localhost:3000");
+    cy.get(".neo-input").should("exist");
+  });
 
-  it('Debe mostrar error con credenciales incorrectas', () => {
-    cy.get('input[type="text"], input[placeholder*="user"], input[name*="user"]')
-      .first()
-      .type('usuariofalso')
-    cy.get('input[type="password"]')
-      .type('clavefalsa')
-    cy.get('button[type="submit"], button').last().click()
-    cy.wait(2000)
-    cy.get('body').should('not.contain', 'Dashboard')
-  })
+  it("Debe mostrar error con credenciales incorrectas", () => {
+    cy.get("#neo-username").type("usuariofalso");
+    cy.get("#neo-password").type("clavefalsa");
+    cy.get("#neo-submit").click();
+    cy.wait(3000);
+    cy.url().should("include", "localhost:3000");
+  });
 
-  it('Debe hacer login exitoso con admin', () => {
-    cy.get('input[type="text"], input[placeholder*="user"], input[name*="user"]')
-      .first()
-      .type('admin')
-    cy.get('input[type="password"]')
-      .type('admin123')
-    cy.get('button[type="submit"], button').last().click()
-    cy.wait(3000)
-    cy.url().should('not.equal', 'http://localhost:3000/')
-  })
-
-})
+  it("Debe hacer login exitoso con admin", () => {
+    cy.get("#neo-username").type("admin");
+    cy.get("#neo-password").type("admin123");
+    cy.get("#neo-submit").click();
+    cy.wait(3000);
+    cy.url().should("not.equal", "http://localhost:3000/");
+  });
+});
